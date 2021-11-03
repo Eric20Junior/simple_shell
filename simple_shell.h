d6ab0445ef45516865be2c9d9bc388c7bdeb7110
Eric20Junior <eric20junior@gmail.com>
2021-11-03 13:22:48 -0700

    simple_shell

--- simple_shell.h
+++ simple_shell.h
@@ -1,93 +1,59 @@
-#ifndef _MAIN_H
-#define _MAIN_H
+#ifndef _SIMPLE_SHELL_H_
+#define _SIMPLE_SHELL_H_
 
+/*libraries*/
 #include <stdio.h>
 #include <stdlib.h>
-#include <signal.h>
-#include <unistd.h>
 #include <string.h>
+#include <unistd.h>
+#include <signal.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include <stdarg.h>
 #include <sys/stat.h>
 
-/**
- * struct exit_status - Struct built
- *
- * @message: The exit message
- * @code: The exit code
- * @exit: 1 if should exit shell, 0 if not.
- */
-typedef struct exit_status
-{
-	char *message;
-	int code;
-	int exit;
-} exit_t;
 
-/**
- * struct built - Struct built
- *
- * @cmd: The command
- * @f: The function associated
- */
-typedef struct built
-{
-	char *cmd;
-	int (*f)(char **arrtok);
-} built_t;
+/*main.c*/
+void INThandler(int sig);
+void print_dollar(void);
 
-/* Declaration of global variables */
-extern char **environ;
+/*find_command.c*/
+unsigned int find_length_command(char *s);
+char **array_strtok(char *str);
 
-/* Integral functions */
-exit_t *proc(char *input, char *ipname, exit_t *estat);
-exit_t *change_status(exit_t *estat, char *msg, int cd, int flag);
-int niproc(char *av[]);
-char *_getenv(const char *name);
-ssize_t _getline(char **lineptr);
-int check_builtins(char *token, char *inputcpy2, char **arrtok);
-exit_t *child_proc(exit_t *estat, char **arrtok, char *cpy2, char *ipname);
-exit_t *pipex(char **argv);
+/*execute.c*/
+void execute(char **commands, char *buffer, char **env,
+		char **argv, int count);
 
-/* Builtin functions */
-int (*get_cmd_func(char *s))(char **arrtok);
-int sharpie_cd(char **arrtok);
-int sharpie_env(char **arrtok);
-int sharpie_exit(char **arrtok);
-int sharpie_setenv(char **arrtok);
-
-/* Environment modification functions */
-char *_getrealenv(const char *name);
-int _setenv(const char *name, const char *value, int overwrite);
-int _unsetenv(const char *name);
-
-/* Memory helper functions */
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-char *null_init(char *buffer, int bufsize);
-void mem_init(int num, ...);
-void minit2(int num, ...);
-
-/* String helper functions */
-int _strcmp(char *s1, char *s2);
-char *_strcat(char *dest, const char *src, const char *delim);
-char *_strcpy(char *src, char *dest);
+/*function_for_strings.c*/
+char *_strncpy(char *dest, char *src, int n);
+char *_strncpyconst(char *dest, const char *src, int n);
+unsigned int _strlen_const(const char *str);
 unsigned int _strlen(char *str);
-int colon_check(char *path, char *command, char **buf, struct stat **bufstat);
+int _strcmp(char *s1, char *s2);
 
-/* Token helper functions */
-char **create_arrtok(char *input, char **arrtok);
-int count_tokens(char *input, const char *delim);
-char *transform_tok(char *command);
-char **tokenize_cmds(char *input, char **cmdtok);
+/*env.c*/
+unsigned int find_num_dir(char *path);
+char **store_e_variables(char *fir_com, char **environ);
+char *_getenv(const char *name, char **environ);
+char *_strncpcommand(char *dest, char *src, char *command, int n, int c);
+void print_env(char **environ);
 
-/* Signal helper functions */
-int _atoi(char *s);
-void check_signal(int sig_num);
+/*free_all.c*/
+void free_all_dp(char **ptr);
+void parent_free_commands(char *buffer, char **commands);
+void send_to_free(char *buffer, char **commands);
 
-/* Other helper functions */
-void get_input(char **input, exit_t *estat);
-void vet_input(int i, char *input, exit_t *estat);
-void _free(unsigned int num, ...);
+/*error_messages.c*/
+void build_message(char **av, char *fir_com, int count);
+int _puterror(char c);
+void end_file(char *buffer);
+void fork_fail(void);
+
+/*child_process.c*/
+void null_command(char *buffer);
+void get_out(char *buffer, char **commands);
+void env_end(char *buffer, char **commands, char **env);
+void _path(char **commands, char *buffer, char **env, char **argv, int count);
+
 
 #endif
